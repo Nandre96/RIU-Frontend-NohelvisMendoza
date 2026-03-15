dfaa9c75e66cd495606322d4853d06dc876efe0a
Nohelvis Mendoza <nohelvismd18@gmail.com>
2026-03-15 11:04:24 -0300

    feat(routes): Define routes for SuperHero components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { SuperHeroDetail } from './heroes/components/super-hero-detail/super-hero-detail';
+import { SuperHeroForm } from './heroes/components/super-hero-form/super-hero-form';
+import { SuperHeroList } from './heroes/components/super-hero-list/super-hero-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'heroes', pathMatch: 'full' },
+  {
+    path: 'heroes',
+    children: [
+      { path: '', loadComponent: () => SuperHeroList },
+      { path: 'new', loadComponent: () => SuperHeroForm },
+      { path: ':slug', loadComponent: () => SuperHeroDetail },
+      { path: ':id/edit', loadComponent: () => SuperHeroForm },
+    ],
+  },
+  { path: '**', redirectTo: 'heroes' },
+];
