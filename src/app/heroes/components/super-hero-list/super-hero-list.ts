ef4e9e537a14fe1fcb4f6b1adac57541458c8ec2
Nohelvis Mendoza <nohelvismd18@gmail.com>
2026-03-15 19:20:18 -0300

    feat(heroes): Implement responsive SuperHeroList component with dynamic grid layout

--- src/app/heroes/components/super-hero-list/super-hero-list.ts
+++ src/app/heroes/components/super-hero-list/super-hero-list.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
-
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Component, computed, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { MatButtonModule, MatIconButton } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { map } from 'rxjs';
+import { SuperHeroViewService } from '../../services/super-hero-view';
 @Component({
   selector: 'app-super-hero-list',
-  imports: [],
+  imports: [
+    MatGridListModule,
+    MatCardModule,
+    MatTooltipModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatIconButton,
+  ],
   templateUrl: './super-hero-list.html',
-  styleUrl: './super-hero-list.css',
+  styleUrls: ['./super-hero-list.css'],
 })
 export class SuperHeroList {
+  private readonly breakpointObserver = inject(BreakpointObserver);
+  readonly superHeroesViewService = inject(SuperHeroViewService);
 
+  readonly #currentBreakpoint = toSignal(
+    this.breakpointObserver
+      .observe([
+        Breakpoints.XSmall,
+        Breakpoints.Small,
+        Breakpoints.Medium,
+        Breakpoints.Large,
+        Breakpoints.XLarge,
+        Breakpoints.Handset,
+        Breakpoints.Tablet,
+        Breakpoints.Web,
+      ])
+      .pipe(
+        map((state: BreakpointState) => {
+          const { XSmall, Small, Medium, Tablet } = state.breakpoints;
+          if (XSmall) return 'XSmall';
+          if (Small) return 'Small';
+          if (Medium) return 'Medium';
+          if (Tablet) return 'Tablet';
+          return 'Other';
+        }),
+      ),
+    { initialValue: 'Other' },
+  );
+
+  readonly gridCols = computed(() => {
+    const breakPoint = this.#currentBreakpoint();
+    if (breakPoint === 'XSmall' || breakPoint === 'Small') return 1;
+    if (breakPoint === 'Medium' || breakPoint === 'Tablet') return 2;
+    return 3;
+  });
 }
