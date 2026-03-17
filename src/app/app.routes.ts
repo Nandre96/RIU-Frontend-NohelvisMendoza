e20161012230c7758dc2084f3c9cab9935e7ef78
Nohelvis Mendoza <nohelvismd18@gmail.com>
2026-03-17 17:34:25 -0300

    refactor(routes): Update route to use lazy-loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,38 @@
 import { Routes } from '@angular/router';
-import { SuperHeroDetail } from './heroes/components/super-hero-detail/super-hero-detail';
-import { SuperHeroForm } from './heroes/components/super-hero-form/super-hero-form';
-import { SuperHeroList } from './heroes/components/super-hero-list/super-hero-list';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'heroes', pathMatch: 'full' },
   {
     path: 'heroes',
     children: [
-      { path: '', loadComponent: () => SuperHeroList },
-      { path: 'new', loadComponent: () => SuperHeroForm },
-      { path: ':slug', loadComponent: () => SuperHeroDetail },
-      { path: ':id/edit', loadComponent: () => SuperHeroForm },
+      {
+        path: '',
+        loadComponent: () =>
+          import('./heroes/components/super-hero-list/super-hero-list').then(
+            (m) => m.SuperHeroList,
+          ),
+      },
+      {
+        path: 'new',
+        loadComponent: () =>
+          import('./heroes/components/super-hero-form/super-hero-form').then(
+            (m) => m.SuperHeroForm,
+          ),
+      },
+      {
+        path: ':slug',
+        loadComponent: () =>
+          import('./heroes/components/super-hero-detail/super-hero-detail').then(
+            (m) => m.SuperHeroDetail,
+          ),
+      },
+      {
+        path: ':id/edit',
+        loadComponent: () =>
+          import('./heroes/components/super-hero-form/super-hero-form').then(
+            (m) => m.SuperHeroForm,
+          ),
+      },
     ],
   },
   { path: '**', redirectTo: 'heroes' },
