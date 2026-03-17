47ed86aa0bffdc388dd63f87b02db56990ced52c
Nohelvis Mendoza <nohelvismd18@gmail.com>
2026-03-17 19:02:22 -0300

    feat(i18n): Translate Angular Material paginator labels to Spanish

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,15 +4,17 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from '@angular/core';
+import { MatPaginatorIntl } from '@angular/material/paginator';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { fakeBackendInterceptor } from './core/interceptors/fake-backend-interceptor';
-
+import { matPaginatorEsFactory } from './shared/i18n/mat-paginator-es';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptors([fakeBackendInterceptor])),
+    { provide: MatPaginatorIntl, useFactory: matPaginatorEsFactory },
   ],
 };
