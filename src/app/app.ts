a3c76219effdda5b32fc93b083708652dedf2ae5
Nohelvis Mendoza <nohelvismd18@gmail.com>
2026-03-17 16:53:51 -0300

    style(app): Refactor app layout with header and responsive styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, MatDividerModule],
   templateUrl: './app.html',
-  styleUrl: './app.css',
+  styleUrls: ['./app.css'],
 })
 export class App {
   protected readonly title = signal('Super Heroes');
