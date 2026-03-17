cf015d0ac5392f6eee6152cf4c0b6b555bcd7214
Nohelvis Mendoza <nohelvismd18@gmail.com>
2026-03-17 17:45:31 -0300

    feat(heroes): Add ErrorMessageForm directive for dynamic error message handling

---
+++ src/app/heroes/directives/error-message-form.spec.ts
@@ -0,0 +1,8 @@
+import { ErrorMessageForm } from './error-message-form';
+
+describe('ErrorMessageForm', () => {
+  it('should create an instance', () => {
+    const directive = new ErrorMessageForm();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/heroes/directives/error-message-form.ts
@@ -0,0 +1,55 @@
+import { Directive, DoCheck, ElementRef, HostBinding, inject, Input } from '@angular/core';
+import { AbstractControl, ControlContainer, ValidationErrors } from '@angular/forms';
+
+@Directive({
+  selector: '[appErrorMessageForm]',
+  standalone: true,
+})
+export class ErrorMessageForm implements DoCheck {
+  @Input() messages: Partial<Record<string, string>> = {};
+
+  private readonly element = inject(ElementRef<HTMLElement>);
+  private readonly controlContainer = inject(ControlContainer, { optional: true });
+
+  private control: AbstractControl | null = null;
+
+  private readonly DEFAULT_MESSAGES: Record<string, string | ((errorContext: any) => string)> = {
+    required: 'Este campo es obligatorio',
+    pattern: 'Formato inválido',
+    min: (errorContext) => `Valor mínimo: ${errorContext.min}`,
+    max: (errorContext) => `Valor máximo: ${errorContext.max}`,
+    minlength: (errorContext) => `Mínimo ${errorContext.requiredLength} caracteres`,
+  };
+
+  @HostBinding('textContent') errorText = '';
+
+  ngDoCheck(): void {
+    if (!this.control) {
+      this.resolveControl();
+    }
+    this.errorText = this.generateErrorMessage();
+  }
+
+  private resolveControl(): void {
+    const matFormField = this.element.nativeElement.closest('mat-form-field');
+    const currentControlElement = matFormField?.querySelector(
+      '[formControlName]',
+    ) as HTMLElement | null;
+    const controlName = currentControlElement?.getAttribute('formControlName');
+
+    if (!controlName) return;
+
+    this.control = this.controlContainer?.control?.get(controlName) ?? null;
+  }
+
+  private generateErrorMessage(): string {
+    if (!this.control || !this.control.touched || !this.control.errors) return '';
+
+    const errorKey = Object.keys(this.control.errors)[0];
+    const errorValue = this.control.errors[errorKey];
+    const template = this.messages[errorKey] ?? this.DEFAULT_MESSAGES[errorKey] ?? 'Campo inválido';
+
+    console.log({ template });
+    return typeof template === 'function' ? template(errorValue) : template;
+  }
+}
