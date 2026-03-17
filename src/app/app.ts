fb40dd488823d032000de457984faa8e5759cf80
Nohelvis Mendoza <nohelvismd18@gmail.com>
2026-03-17 17:42:58 -0300

    feat(app): Add navigation link to header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
-import { RouterOutlet } from '@angular/router';
+import { RouterLinkWithHref, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, MatDividerModule],
+  imports: [RouterOutlet, CommonModule, MatDividerModule, RouterLinkWithHref, RouterModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
 })
