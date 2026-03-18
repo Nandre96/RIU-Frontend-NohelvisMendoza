1f77474fd4271d4fa1672435861a005a9d36f064
Nohelvis Mendoza <nohelvismd18@gmail.com>
2026-03-17 21:12:28 -0300

    feat(heroes): Add missing id property to power level metadata constants

--- src/app/heroes/models/const/power-level.const.ts
+++ src/app/heroes/models/const/power-level.const.ts
@@ -2,26 +2,31 @@ import { PowerLevelEnum } from '../enum/power-level.enum';
 import { PowerLevelView } from '../interfaces/view/power-level.view.interface';
 
 const LOW_POWER_LEVEL_META: Readonly<PowerLevelView> = {
+  id: PowerLevelEnum.LOW,
   label: 'Bajo',
   color: '#10B981',
 };
 
 const MEDIUM_POWER_LEVEL_META: Readonly<PowerLevelView> = {
+  id: PowerLevelEnum.MEDIUM,
   label: 'Medio',
   color: '#D97706',
 };
 
 const HIGH_POWER_LEVEL_META: Readonly<PowerLevelView> = {
+  id: PowerLevelEnum.HIGH,
   label: 'Alto',
   color: '#EA580C',
 };
 
 const GODLIKE_POWER_LEVEL_META: Readonly<PowerLevelView> = {
+  id: PowerLevelEnum.GODLIKE,
   label: 'Godlike',
   color: '#DC2626',
 };
 
 export const UNKNOWN_POWER_LEVEL_META: Readonly<PowerLevelView> = {
+  id: PowerLevelEnum.UNKNOWN,
   label: 'Desconocido',
   color: '#6B7280',
 };
