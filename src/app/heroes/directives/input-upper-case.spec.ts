b122c3cd948161944d030640a94a157725575bd8
Nohelvis Mendoza <nohelvismd18@gmail.com>
2026-03-19 14:38:53 -0300

    chore(heroes): Delete .spec files

--- src/app/heroes/directives/input-upper-case.spec.ts
+++
@@ -1,8 +0,0 @@
-import { InputUpperCase } from './input-upper-case';
-
-describe('InputUpperCase', () => {
-  it('should create an instance', () => {
-    const directive = new InputUpperCase();
-    expect(directive).toBeTruthy();
-  });
-});
